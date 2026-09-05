1b54193985c64bd3e38eb5e9fe8388c503b7f7ee
Karan sapra <88654249+KaranSapra124@users.noreply.github.com>
2026-09-06 02:15:44 +0530

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        // let left = 0,
+        //     right = numbers.length - 1,
+        //     mid = 0;
+
+        // while (left < right) {
+        //     if (numbers[left] + numbers[right] === target) return [left + 1, right + 1];
+        //     else if (numbers[left] + numbers[right] > target) right--;
+        //     else if (numbers[left] + numbers[right] < target) left++;
+        // }
+        for (let i = 0; i < numbers.length; i++) {
+            const needed = target - numbers[i];
+            let left = i + 1;
+            let right = numbers.length - 1;
+            while (left <= right) {
+                const mid = Math.floor((left + right) / 2);
+
+                if (numbers[mid] === needed) {
+                    return [i + 1, mid + 1];
+                }
+
+                if (numbers[mid] < needed) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return [];
+    }
+}
