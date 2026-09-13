14a38a98ebb6423fef2681955149be69c62f71db
Karan sapra <88654249+KaranSapra124@users.noreply.github.com>
2026-09-13 15:39:52 +0530

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+       const deque = [], arr = []
+    for (let i = 0; i < nums.length; i++) {
+        if (deque.length  && deque[0] < i - k + 1) {
+            deque.shift()
+        }
+        while (deque.length  && nums[deque[deque.length - 1]] <= nums[i]) {
+            deque.pop()
+        }
+        deque.push(i)
+        if (i >= k - 1) {
+            arr.push(nums[deque[0]])
+        }
+    }
+    return arr
+    }
+}
