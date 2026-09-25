145d9ea355f9e68d1e8bbf388ffba469dabdb9af
Karan sapra <88654249+KaranSapra124@users.noreply.github.com>
2026-09-26 00:16:30 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const cars = [];
+        for (let i = 0; i < position.length; i++) {
+            const time = (target - position[i]) / speed[i];
+            cars.push([position[i], time]);
+        }
+        cars.sort((a, b) => b[0] - a[0]);
+        console.log(cars)
+        let fleet = 0;
+        let lastTime = 0;
+
+        for (const [position, time] of cars) {
+            if (time > lastTime) {
+                fleet++;
+                lastTime = time;
+            }
+        }
+        return fleet;
+    }
+}
